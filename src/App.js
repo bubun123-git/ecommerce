9dfae3c7af9c36da554c7167ca0339738155fce9
bubun123-git <bubundcool@gmail.com>
2023-08-12 12:23:59 +0530

    worked on cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+
 import Product from './Components/Product';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ecommerce React</h1>
+      <h1>THE GENERICS</h1>
+      
       <Product></Product>
     </div>
   );
--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,62 +1,46 @@
+import React from 'react';
+
 function Product() {
-
     const productsArr = [
-
         {
-
             title: 'Colors',
-
             price: 100,
-
             imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-
         },
-
         {
-
             title: 'Black and white Colors',
-
             price: 50,
-
             imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-
         },
-
         {
-
             title: 'Yellow and Black Colors',
-
             price: 70,
-
             imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-
         },
-
         {
-
             title: 'Blue Color',
-
             price: 100,
-
             imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-
         }
-
-    ]
+    ];
 
     const LoopProduct = productsArr.map((item, index) => (
         <li key={index}>
             <h2>{item.title}</h2>
             <p>Price: ${item.price}</p>
             <img src={item.imageUrl} alt={item.title} />
+            <br></br>
+            <button>Add to Cart</button>
         </li>
     ));
+
     return (
         <div>
-            {LoopProduct}
+            <ul>
+                {LoopProduct}
+            </ul>
         </div>
-    )
-
-
+    );
 }
+
 export default Product;
